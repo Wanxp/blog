295615ebec0d1be67e0d9b00cbca7e865879972c
wanxp <977741432@qq.com>
2019-11-20 13:10:43 +0800

    后台初步功能

--- src/main/java/com/wanxp/blog/controller/AdminController.java
+++ src/main/java/com/wanxp/blog/controller/AdminController.java
@@ -1,19 +1,10 @@
 package com.wanxp.blog.controller;
 
-import com.wanxp.blog.model.dto.PlatformResult;
-import com.wanxp.blog.model.dto.UserDTO;
-import com.wanxp.blog.model.vo.UserVO;
-import com.wanxp.blog.service.UserService;
-import com.wanxp.blog.util.MyBeanUtils;
-import com.wanxp.blog.validation.groups.Login;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.wanxp.blog.model.vo.StatisticsVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import static com.wanxp.blog.constant.BlogConst.BLOG_TITLE;
 
@@ -25,7 +16,7 @@ public class AdminController {
     @GetMapping("index")
     public String index(Model model) {
         model.addAttribute("title", BLOG_TITLE);
-        model.addAttribute("statistics", null);
+        model.addAttribute("statistics", new StatisticsVO(0, 0, 0));
         model.addAttribute("articles", null);
         model.addAttribute("components", null);
         model.addAttribute("logs", null);
--- src/main/java/com/wanxp/blog/model/vo/StatisticsVO.java
+++ src/main/java/com/wanxp/blog/model/vo/StatisticsVO.java
@@ -1,8 +1,10 @@
 package com.wanxp.blog.model.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class StatisticsVO {
     private Integer articles;
     private Integer comments;
